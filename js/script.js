6b97bf480dc8754686c27e2bc332b1c57e289b32
ErfanParasti <108697797+ErfanParasti@users.noreply.github.com>
2022-12-21 19:18:58 +0330

    Upload simple PWA project on Github

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const f = document.getElementById('form');
+
+const q = document.getElementById('query');
+
+const google = 'https://www.google.com/search?q=site%3A+';
+
+const site = 'soft98.ir';
+
+
+
+
+function submitted(event) {
+
+  event.preventDefault();
+
+  const url = google + site + '+' + q.value;
+
+  const win = window.open(url, '_blank');
+
+  win.focus();
+
+}
+
+
+
+
+f.addEventListener('submit', submitted);
